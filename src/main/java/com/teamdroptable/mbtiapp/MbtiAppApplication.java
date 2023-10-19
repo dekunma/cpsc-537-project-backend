22b2e3d51168077cb8177e378fc0391911945933
Dekun Ma <william@dekun.me>
2023-10-19 14:09:44 -0400

    Add swagger 2

--- src/main/java/com/teamdroptable/mbtiapp/MbtiAppApplication.java
+++ src/main/java/com/teamdroptable/mbtiapp/MbtiAppApplication.java
@@ -2,8 +2,12 @@ package com.teamdroptable.mbtiapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableWebMvc
 public class MbtiAppApplication {
 
 	public static void main(String[] args) {
