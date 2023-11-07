dec4326bdc9bdd6293546e60b806f74b10ec20a1
Dekun Ma <william@dekun.me>
2023-11-07 12:32:54 -0500

    Remove unit tests

--- src/test/java/com/teamdroptable/mbtiapp/MbtiAppApplicationTests.java
+++ src/test/java/com/teamdroptable/mbtiapp/MbtiAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MbtiAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
