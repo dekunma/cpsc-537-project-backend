a0e7625c7207031ed4988d551ae169f1597d6f40
Dekun Ma <william@dekun.me>
2023-11-09 16:50:16 -0500

    Shuffle people on random 10 people API

--- src/main/java/com/teamdroptable/mbtiapp/service/MbtiService.java
+++ src/main/java/com/teamdroptable/mbtiapp/service/MbtiService.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -44,6 +45,7 @@ public class MbtiService {
                     .description(p.getDescription100() == null ? p.getMovie() : p.getDescription100())
                     .build());
 
+        Collections.shuffle(ret);
         return ret;
     }
 }
